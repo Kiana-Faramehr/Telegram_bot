ef4feee2f99c8a59af6fc87fbd406f48f97c51a0
Aryan-Fartash <fartasharyan@gmail.com>
2024-11-29 03:44:52 -0700

    Shaping my bot and testing the supervisor part
    -completed start and custom function
    -checking Computer_science and supervisor functions with small database

---
+++ list.py
@@ -0,0 +1,63 @@
+def data():
+    list_doc=["""
+👨‍🏫 Supervisor Information
+
+• Supervisor: Dr N Zincir-Heywood
+
+🎓 Details of Open Research Position
+
+👤 Cyber Security and Resilience
+
+🏅 Level: PhD
+🌍 Country: Canada
+🏛 University: Dalhousie University
+🔬 Branch: Faculty of Computer Science
+
+📝 Overview:
+In this research project, we are going to work on monitoring and analysis of adversity and changes in the communication networks and services using machine learning and artificial intelligence approaches.
+
+🏷 Tags:
+• Artificial Intelligence
+• Cyber Security
+• Machine Learning
+• Networks
+• Computer Science
+
+🔗 Online Profiles:
+• <a href='https://ca.linkedin.com/in/nur-zincir-heywood-30a4456'>LinkedIn</a>
+
+• <a href='https://scholar.google.com/citations?user=F9nG0F4AAAAJ&hl=zh-CN'>Google Scholar</a>
+
+    """,
+
+
+
+    """
+    👨‍🏫 Supervisor Information
+
+• Supervisor: Dr D Cafolla
+
+🎓 Details of Open Research Position
+
+👤 Computer Science: Fully Funded EPSRC DTP PhD Scholarship: Vertical Multi-Purpose Farming Robotic System
+
+🏅 Level: PhD
+🌍 Country: United Kingdom
+🏛 University: Swansea University
+🔬 Branch: Department of Computer Science
+
+📝 Overview:
+Funding provider(s):  . Engineering and Physical Sciences Research Council (EPSRC) Doctoral Training Partnerships (DTP) and Swansea University's Faculty of Science and Engineering.
+
+🏷 Tags:
+• Computer Science
+
+🔗 Online Profiles:
+• <a href='https://www.linkedin.com/in/dcafolla'>LinkedIn</a>
+
+• <a href='https://scholar.google.com/citations?user=cefQLJkAAAAJ&hl=en'>Google Scholar</a>
+
+• <a href='https://www.swansea.ac.uk/postgraduate/scholarships/research/computer-science-epsrc-phd-farming-robotic-system-rs676.php?utm_source=findaphd&utm_medium=scholarship&utm_campaign=postgraduate24&utm_content=pgr'>🌐 Institution Page</a>
+    """,
+    ]
+    return list_doc
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram import Update,KeyboardButton,ReplyKeyboardMarkup,InlineKeyboardBut
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from telegram.constants import ParseMode
 import requests
-
+from list import data
 
 TOKEN: Final='7966714900:AAFxGbdeW4ijfsaszt1BTpbgs_8cRDZnpqY'
 BOT_USERNAME: Final='@abroadin_apply_bot'
@@ -11,55 +11,56 @@ BOT_USERNAME: Final='@abroadin_apply_bot'
 
 #Commands
 async def start_command(update: Update, contex: ContextTypes.DEFAULT_TYPE):
-    await contex.bot.send_message(chat_id=update._effective_chat.id,text="Hello! Welcome to the bot! 🌟We’re here to support you on your academic journey. Please choose an option from the main menu to get started! It’s recommended to complete your user profile first in the «User Profile» section. 😊")
+        
+    #buttons
+    keys=[  [KeyboardButton('👤 User Profile')],
+            [KeyboardButton('❓ Help and Support'),KeyboardButton('💾 Saved Results')],
+            [KeyboardButton('📋 View/Edit Profile'),KeyboardButton('🧑‍🏫 Search Supervisors')]]
+    
+    await contex.bot.send_message(chat_id=update.effective_chat.id,
+        text="Hello! Welcome to the bot! 🌟We’re here to support you on your academic journey. Please choose an option from the main menu to get started! It’s recommended to complete your user profile first in the «User Profile» section. 😊",
+        reply_markup=ReplyKeyboardMarkup(keys, resize_keyboard=True))
 
-async def Supervisors(update: Update, contex: ContextTypes.DEFAULT_TYPE):
+async def supervisors(update: Update, contex: ContextTypes.DEFAULT_TYPE):
+    
+    #buttons
+    keys=[[KeyboardButton('Computer Science')]]
+    await contex.bot.send_message(chat_id=update.effective_chat.id,
+        text="Dear l, To see a list of supervisors, please enter a keyword related to your research interest or field of study",
+        reply_markup=ReplyKeyboardMarkup(keys, resize_keyboard=True))
+
+
+async def computer_science(update: Update, contex: ContextTypes.DEFAULT_TYPE):
 
     button_list=[[InlineKeyboardButton('save',url='t.me/aryan_fr1')]]
+    dt=data()
+    for i in range(len(data())):
+        await contex.bot.send_message(chat_id=update.effective_chat.id,
+            text=dt[i],
+            parse_mode=ParseMode.HTML,
+            reply_markup=InlineKeyboardMarkup(button_list))
+
+
+async def custom(update: Update, contex: ContextTypes.DEFAULT_TYPE):
+    msg=update.effective_message.text
+    if msg=="🧑‍🏫 Search Supervisors":
+        await supervisors(update,contex)
+    elif msg=="Computer Science":
+        await computer_science(update,contex)
 
 
 
-    txt="""
-👨‍🏫 Supervisor Information
 
-• Supervisor: Dr N Zincir-Heywood
-
-🎓 Details of Open Research Position
-
-👤 Cyber Security and Resilience
-
-🏅 Level: PhD
-🌍 Country: Canada
-🏛 University: Dalhousie University
-🔬 Branch: Faculty of Computer Science
-
-📝 Overview:
-In this research project, we are going to work on monitoring and analysis of adversity and changes in the communication networks and services using machine learning and artificial intelligence approaches.
-
-🏷 Tags:
-• Artificial Intelligence
-• Cyber Security
-• Machine Learning
-• Networks
-• Computer Science
-
-🔗 Online Profiles:
-• <a href='https://ca.linkedin.com/in/nur-zincir-heywood-30a4456'>LinkedIn</a>
-
-• <a href='https://scholar.google.com/citations?user=F9nG0F4AAAAJ&hl=zh-CN'>Google Scholar</a>
-
-"""
-    await contex.bot.send_message(chat_id=update.effective_chat.id,text=txt,parse_mode=ParseMode.HTML,reply_markup=InlineKeyboardMarkup(button_list))
 
 if __name__=='__main__':
     print("starting bot...")
     app = Application.builder().token(TOKEN).build()
 
-    #commands
+
     app.add_handler(CommandHandler('start', start_command))
-    #app.add_handler(CommandHandler('help', help_command))
-    #app.add_handler(CommandHandler('custom', custom_command))
-    app.add_handler(CommandHandler('Supervisor', Supervisors))
+    app.add_handler(CommandHandler('computer_science', computer_science))
+    app.add_handler(CommandHandler('Supervisor', supervisors))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND) ,  custom))
 
 
     print('polling...')
